fdea95daccf64480cd7408c83c5f3c9775fe324b
Richard Neumann <r.neumann@homeinfo.de>
2021-12-01 11:23:21 +0100

    Added basic ORM

--- ccmessenger/__init__.py
+++ ccmessenger/__init__.py
@@ -1,1 +1,6 @@
 """ComCat tenant <-> landlord messenger."""
+
+from ccmessenger.orm import Attachment, Message
+
+
+__all__ = ['Attachment', 'Message']
---
+++ ccmessenger/orm.py
@@ -0,0 +1,55 @@
+"""Object-relational mappings."""
+
+from __future__ import annotations
+from datetime import datetime
+
+from comcatlib import User
+from filedb import File
+from mdb import Customer
+from peewee import DateTimeField, ForeignKeyField, TextField
+
+from peeweeplus import JSONModel, MySQLDatabase
+
+
+__all__ = ['Message', 'Attachment']
+
+
+DATABASE = MySQLDatabase()
+
+
+class CCMessengerModel(JSONModel):  # pylint: disable=R0903
+    """Base model for this database."""
+
+    class Meta:     # pylint: disable=R0903,C0115
+        database = DATABASE
+        schema = database.database
+
+
+class Message(CCMessengerModel):
+    """A Message."""
+
+    parent = ForeignKeyField('self', column_name='parent', null=True)
+    user = ForeignKeyField(User, column_name='user', null=True,
+                           on_delete='CASCADE')
+    customer = ForeignKeyField(Customer, column_name='customer', null=True,
+                               on_delete='CASCADE')
+    text = TextField()
+    created = DateTimeField(default=datetime.now)
+
+    @classmethod
+    def for_user(cls, user: User, text: str) -> Message:
+        """Creates a message for the given user."""
+        return cls(user=user, text=text)
+
+    @classmethod
+    def for_customer(cls, customer: Customer, text: str) -> Message:
+        """Creates a message for the given customer."""
+        return cls(customer=customer, text=text)
+
+
+class Attachment(CCMessengerModel):     # pylint: disable=R0903
+    """A file attachment."""
+
+    message = ForeignKeyField(Message, column_name='message',
+                              on_delete='CASCADE')
+    file = ForeignKeyField(File, column_name='file')
