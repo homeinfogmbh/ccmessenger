738da166f0727698ddaeda8d1d7dbcb52073c236
Richard Neumann <r.neumann@homeinfo.de>
2021-12-01 09:58:52 +0100

    Add setup.py and Makefile

---
+++ setup.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name='ccmessenger',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'configlib',
+        'emaillib',
+        'flask',
+        'functoolsplus',
+        'mdb',
+        'notificationlib',
+        'peewee',
+        'peeweeplus',
+        'wsgilib'
+    ],
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='<info at homeinfo dot de>',
+    maintainer='Richard Neumann',
+    maintainer_email='<r dot neumann at homeinfo priod de>',
+    packages=['ccmessenger'],
+    license='GPLv3',
+    description='ComCat tenant <-> landlord messenger.'
+)
