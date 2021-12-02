c53a86a8f0bb13e0be5b6944da92c3c326bec5fc
Richard Neumann <r.neumann@homeinfo.de>
2021-12-02 10:11:03 +0100

    Add common functions interface

--- ccmessenger/__init__.py
+++ ccmessenger/__init__.py
@@ -1,6 +1,21 @@
 """ComCat tenant <-> landlord messenger."""
 
+from ccmessenger.functions import get_customer_messages
+from ccmessenger.functions import get_customer_message
+from ccmessenger.functions import get_own_messages
+from ccmessenger.functions import get_own_message
+from ccmessenger.functions import get_user_messages
+from ccmessenger.functions import get_user_message
 from ccmessenger.orm import Attachment, Message
 
 
-__all__ = ['Attachment', 'Message']
+__all__ = [
+    'Attachment',
+    'Message',
+    'get_customer_messages',
+    'get_customer_message',
+    'get_own_messages',
+    'get_own_message',
+    'get_user_messages',
+    'get_user_message'
+]
---
+++ ccmessenger/functions.py
@@ -0,0 +1,63 @@
+"""Common functions."""
+
+from typing import Union
+
+from peewee import ModelSelect
+
+from comcatlib import User
+from mdb import Address, Company, Customer, Tenement
+
+from ccmessenger.orm import Message, Attachment
+
+
+__all__ = [
+    'get_customer_messages',
+    'get_customer_message',
+    'get_own_messages',
+    'get_own_message',
+    'get_user_messages',
+    'get_user_message'
+]
+
+
+def get_customer_messages(customer: Union[Customer, int]) -> ModelSelect:
+    """Selects messages of the given customer."""
+
+    return Message.select(Message, Customer, Company, Attachment).join(
+        Customer).join(Company).join_from(Message, Attachment).where(
+        Message.customer == customer)
+
+
+def get_customer_message(ident: int, customer: Union[Customer, int]) \
+        -> Message:
+    """Returns a customer message."""
+
+    return get_customer_messages(customer).where(Message.id == ident).get()
+
+
+def get_own_messages(user: Union[User, int]) -> ModelSelect:
+    """Selects messages of the given user."""
+
+    return Message.select(Message, User, Tenement, Address, Attachment).join(
+        User).join(Tenement).join(Address).join_from(
+        Message, Attachment).where(Message.user == user)
+
+
+def get_own_message(ident: int, user: Union[User, int]) -> Message:
+    """Returns a user message."""
+
+    return get_own_messages(user).where(Message.id == ident).get()
+
+
+def get_user_messages(customer: Union[Customer, int]) -> ModelSelect:
+    """Selects user messages of the given customer."""
+
+    return Message.select(Message, User, Tenement, Address, Attachment).join(
+        User).join(Tenement).join(Address).join_from(
+        Message, Attachment).where(Tenement.customer == customer)
+
+
+def get_user_message(ident: int, customer: Union[Customer, int]) -> Message:
+    """Returns a user message of the give customer."""
+
+    return get_user_messages(customer).where(Message.id == ident).get()
